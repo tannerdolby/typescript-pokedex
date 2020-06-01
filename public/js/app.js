2d899035b26b7428f77cb0e9b2c8bfd649dbdfb9
Tanner <dolb.tanner@gmail.com>
2020-06-01 11:16:50 -0700

    Changed API fetch and ordered CSS Grid with .sort()

--- public/js/app.js
+++ public/js/app.js
@@ -9,52 +9,52 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const container = document.body.querySelector("#app");
-const card = document.body.querySelector(".poke-card");
-const pokemon = 120;
-let fetchAPIData = () => {
-    for (let i = 1; i <= pokemon; i++) {
-        getPokemon(i);
-    }
+// const searchBar: HTMLElement | any = document.body.querySelector("#searchBar");
+// const searchBtn: HTMLElement | any = document.body.querySelector("#search-btn");
+const pokemonToFetch = 151;
+const newPokeImg = function (pokeID) {
+    let src = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
+    return src;
 };
-const getPokemon = (id) => __awaiter(void 0, void 0, void 0, function* () {
-    const data = yield fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const pokemon = yield data.json();
-    const pokemonType = pokemon.types
-        .map((pokemon) => pokemon.type.name)
-        .join(", ");
-    const pokemonAbilities = pokemon.abilities
-        .map((pokemon) => pokemon.ability.name)
-        .join(", ");
-    const pokemonHP = pokemon.stats
-        .map((pokemon) => pokemon.stat.name).slice(pokemon.stats.length - 1)
-        .join(" ");
-    const pokemonStats = pokemon.stats
-        .map((pokemon) => pokemon.base_stat).slice(pokemon.stats.length - 1)
-        .join(" ");
-    const firstChar = pokemon.name.slice(0, 1).toUpperCase();
-    const restOfStr = pokemon.name.slice(1).toLowerCase();
-    const titleCasedName = firstChar.concat(restOfStr);
-    const transformedPokemon = {
-        id: pokemon.id,
-        order: pokemon.order,
-        name: titleCasedName,
-        stat: pokemonStats,
-        base_stat: pokemonHP,
-        image: `${pokemon.sprites.front_default}`,
-        type: pokemonType,
-        weight: pokemon.weight,
-        height: pokemon.height,
-        abilities: pokemonAbilities,
-    };
-    showPokemon(transformedPokemon);
-});
-/* Render HTML */
-const showPokemon = (pokemon) => {
-    let output = `
-        <div class="poke-card" id="${pokemon.name}">
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= pokemonToFetch; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results
+            .map((result) => ({
+            id: result.id,
+            name: result.name
+                .slice(0, 1)
+                .toUpperCase()
+                .concat(result.name.slice(1).toLowerCase()),
+            type: result.types.map((poke) => poke.type.name),
+            image: newPokeImg(result.id),
+            weight: result.weight,
+            height: result.height,
+            base_stat: result.stats
+                .map((poke) => poke.base_stat)
+                .slice(result.stats.length - 1)
+                .join(" "),
+            stat: result.stats
+                .map((poke) => poke.stat.name)
+                .slice(result.stats.length - 1)
+                .join(" ")
+                .toUpperCase()
+        }))
+            .sort((a, b) => a.id - b.id);
+        displayPokemon(pokemon);
+    });
+};
+const displayPokemon = (pokemon) => {
+    const pokemonHTMLString = pokemon
+        .map((pokemon) => `
+            <div class="poke-card" id="${pokemon.name}">
             <div class="flexy">
-                <span class="card-id">#${pokemon.id} </span>
-                <span class="card-hp"><i id="poke-hp" class="fa fa-heart" aria-hidden="true"></i>${pokemon.stat}${pokemon.base_stat}</span>
+                <span class="card-id">#${pokemon.id}</span>
+                <span class="card-hp">${pokemon.base_stat} ${pokemon.stat} <i id="poke-hp" class="fa fa-heart" aria-hidden="true"></i></span>
             </div>
             <h1 class="card-name">${pokemon.name}</h1>
             <img class="card-image" src=${pokemon.image} alt=${pokemon.name} />
@@ -62,7 +62,21 @@ const showPokemon = (pokemon) => {
             <span>Length: ${pokemon.height} in, Weight: ${pokemon.weight} lbs.</span>
             <!-- <span>Abilities: ${pokemon.abilities}</span> -->
         </div>
-    `;
-    container.innerHTML += output;
+        `)
+        .join("");
+    container.innerHTML = pokemonHTMLString;
 };
-fetchAPIData();
+fetchPokemon();
+// function to iterate through all json response up until the number of pokemon specified to fetch
+const hitAPI = () => {
+    for (let i = 1; i <= pokemonToFetch; i++) {
+        mySearchFetch(i);
+    }
+};
+// fetches a single pokemon or response result from poke api
+const mySearchFetch = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const fetcher = yield fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemon = yield fetcher.json();
+    return pokemon;
+});
+hitAPI();
--- src/app.ts
+++ src/app.ts
@@ -1,75 +1,54 @@
-const container: HTMLElement | any = document.body.querySelector("#app")
-const card: HTMLElement | any = document.body.querySelector(".poke-card");
-const pokemon: number = 120;
+const container: HTMLElement | any = document.body.querySelector("#app");
+// const searchBar: HTMLElement | any = document.body.querySelector("#searchBar");
+// const searchBtn: HTMLElement | any = document.body.querySelector("#search-btn");
+const pokemonToFetch: number = 151;
 
+const newPokeImg = function(pokeID: any) {
+  let src = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
+  return src;
+};
 
-interface IPokemon {
-    id: number;
-    order: number;
-    name: string;
-    image: string;
-    type: string;
-    stat: string;
-    base_stat: string;
-    weight: number;
-    height: number;
-    abilities: string;
-}
+const fetchPokemon = () => {
+  const promises = [];
+  for (let i = 1; i <= pokemonToFetch; i++) {
+    const url: string = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then((res: any) => res.json()));
+  }
+  Promise.all(promises).then((results: any) => {
+    const pokemon = results
+      .map((result: any) => ({
+        id: result.id,
+        name: result.name
+          .slice(0, 1)
+          .toUpperCase()
+          .concat(result.name.slice(1).toLowerCase()),
+        type: result.types.map((poke: any) => poke.type.name),
+        image: newPokeImg(result.id),
+        weight: result.weight,
+        height: result.height,
+        base_stat: result.stats
+          .map((poke: any) => poke.base_stat)
+          .slice(result.stats.length - 1)
+          .join(" "),
+        stat: result.stats
+          .map((poke: any) => poke.stat.name)
+          .slice(result.stats.length - 1)
+          .join(" ")
+          .toUpperCase()
+      }))
+      .sort((a: any, b: any) => a.id - b.id);
+    displayPokemon(pokemon);
+  });
+};
 
-let fetchAPIData = (): void => {
-    for (let i = 1; i <= pokemon; i++) {
-        getPokemon(i)
-    }
-}
-
-const getPokemon = async (id: number): Promise<void> => {
-    const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    const pokemon: any = await data.json()
-
-    
-    const pokemonType: string = pokemon.types
-        .map((pokemon: any) => pokemon.type.name)
-        .join(", ")
-
-    const pokemonAbilities: string = pokemon.abilities
-        .map((pokemon: any) => pokemon.ability.name)
-        .join(", ");
-
-    const pokemonHP: string = pokemon.stats
-        .map((pokemon: any) => pokemon.stat.name).slice(pokemon.stats.length -1)
-        .join(" ");
-    
-    const pokemonStats: string = pokemon.stats
-        .map((pokemon: any) => pokemon.base_stat).slice(pokemon.stats.length-1)
-        .join(" ");
-
-
-    const firstChar: string = pokemon.name.slice(0, 1).toUpperCase();
-    const restOfStr: string = pokemon.name.slice(1).toLowerCase();
-    const titleCasedName: string = firstChar.concat(restOfStr);
-
-    const transformedPokemon = {
-        id: pokemon.id,
-        order: pokemon.order,
-        name: titleCasedName,
-        stat: pokemonStats,
-        base_stat: pokemonHP,
-        image: `${pokemon.sprites.front_default}`,
-        type: pokemonType,
-        weight: pokemon.weight,
-        height: pokemon.height,
-        abilities: pokemonAbilities,
-    }
-    showPokemon(transformedPokemon);
-}
-
-/* Render HTML */
-const showPokemon = (pokemon: IPokemon) : void => {
-    let output: string = `
-        <div class="poke-card" id="${pokemon.name}">
+const displayPokemon = (pokemon: any) => {
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokemon: any) => `
+            <div class="poke-card" id="${pokemon.name}">
             <div class="flexy">
-                <span class="card-id">#${pokemon.id} </span>
-                <span class="card-hp"><i id="poke-hp" class="fa fa-heart" aria-hidden="true"></i>${pokemon.stat}${pokemon.base_stat}</span>
+                <span class="card-id">#${pokemon.id}</span>
+                <span class="card-hp">${pokemon.base_stat} ${pokemon.stat} <i id="poke-hp" class="fa fa-heart" aria-hidden="true"></i></span>
             </div>
             <h1 class="card-name">${pokemon.name}</h1>
             <img class="card-image" src=${pokemon.image} alt=${pokemon.name} />
@@ -77,8 +56,26 @@ const showPokemon = (pokemon: IPokemon) : void => {
             <span>Length: ${pokemon.height} in, Weight: ${pokemon.weight} lbs.</span>
             <!-- <span>Abilities: ${pokemon.abilities}</span> -->
         </div>
-    `
-    container.innerHTML += output;
-}
+        `
+    )
+    .join("");
+  container.innerHTML = pokemonHTMLString;
+};
+fetchPokemon();
 
-fetchAPIData();
+// function to iterate through all json response up until the number of pokemon specified to fetch
+const hitAPI = (): void => {
+  for (let i = 1; i <= pokemonToFetch; i++) {
+    mySearchFetch(i);
+  }
+};
+
+// fetches a single pokemon or response result from poke api
+const mySearchFetch = async (id: number): Promise<void> => {
+  const fetcher: Response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${id}`
+  );
+  const pokemon: any = await fetcher.json();
+  return pokemon;
+};
+hitAPI();
